2b786cda5264001360be4cd86d579c696f2d9bc8
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-01-01 21:33:53 -0800

    script.js

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const boardContainer = document.getElementById('boardcont');
 let boxes = document.querySelectorAll('.markercont');
 
 const playerOne = {
-    name: 'User',
+    name: 'Player One',
     marker: 'X'
 };
 
 const playerTwo = {
-    name: 'Computer',
+    name: 'Player Two',
     marker: 'O'
 };
 
-let currentPlayer = playerTwo;
+//sets playerOne as the default first player
+let currentPlayer = playerOne;
+
 let boardArray = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'O', 'X'];
 
 //this assigns each box an individual ID...
@@ -22,20 +24,36 @@ for (i = 0; i < boxes.length; i++) {
 };
 
 
-function markBoard() {
-    for (i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('click', function() {
-            switch (currentPlayer) {
-                case playerOne:
-                    this.innerHTML = playerOne.marker;
-                    break;
-                case playerTwo:
-                    this.innerHTML = playerTwo.marker;
-                    break;
-            };
-        });
-    };
+//controls the actual "marking" of Xs and Os on the board...
+for (i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', function() {
+        switch (currentPlayer) {
+            case playerOne:
+                this.innerHTML = playerOne.marker;
+                switchActivePlayer();
+                break;
+            case playerTwo:
+                this.innerHTML = playerTwo.marker;
+                switchActivePlayer();
+                break;
+        };
+    });
 };
 
-markBoard();
+//switches the active player and populates current player display on DOM
+function switchActivePlayer() {
+    switch(currentPlayer) {
+        case playerOne:
+            currentPlayer = playerTwo;
+            document.getElementById('currentTurnDisplay').innerHTML = 'Player Two (O)';
+            break;
+        case playerTwo:
+            currentPlayer = playerOne;
+            document.getElementById('currentTurnDisplay').innerText = 'Player One (X)';
+            break;
+    }
+};
 
+function gameFlow() {
+
+}
