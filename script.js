f39125367ae3a6fcb169f89ba05cee305909d90a
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-01-01 19:37:23 -0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const boardContainer = document.getElementById('boardcont');
 let boxes = document.querySelectorAll('.markercont');
-let currentLetter = 'X';
 
 const playerOne = {
     name: 'User',
@@ -12,16 +11,31 @@ const playerTwo = {
     marker: 'O'
 };
 
+let currentPlayer = playerTwo;
 let boardArray = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'O', 'X'];
 
+//this assigns each box an individual ID...
+let idMaker = 0;
 for (i = 0; i < boxes.length; i++) {
-    boxes[i].innerHTML = boardArray[i];
-    /*
-    boxes[i].addEventListener('click', function() {
-        this.innerHTML = boardArray[i];
-    })
-    */
+    boxes[i].id = idMaker;
+    idMaker++;
 };
 
 
+function markBoard() {
+    for (i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('click', function() {
+            switch (currentPlayer) {
+                case playerOne:
+                    this.innerHTML = playerOne.marker;
+                    break;
+                case playerTwo:
+                    this.innerHTML = playerTwo.marker;
+                    break;
+            };
+        });
+    };
+};
+
+markBoard();
 
